4f9470be6cd3467723c23edcf1d7329c5ba26334
HapfuKei <hapfu.kei@gmail.com>
2018-11-21 17:14:26 +0200

    version 1.0 Sem Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 function openForm() {
   document.getElementById("myForm").style.display = "block";
+  document.getElementById("aside1").style.display = "none";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+  document.getElementById("aside1").style.display = "";
 }
-

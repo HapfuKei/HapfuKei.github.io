b85f4c3bbe3a56672e46b3aee19eee78dd01efa0
HapfuKei <hapfu.kei@gmail.com>
2018-11-21 19:36:54 +0200

    To top button animation

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,12 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
   document.getElementById("aside1").style.display = "";
 }
+var t;
+function up() {
+  var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+  if(top > 0) {
+    window.scrollBy(0,-170);
+    t = setTimeout('up()',20);
+  } else clearTimeout(t);
+  return false;
+}
